443fd2869ff38ee83924ba30841694ea60afdcf8
Jun <wnsgur4092@gmail.com>
2022-08-31 01:16:52 +1000

    Initial Commit

---
+++ StockRank-SwiftUI/StockRank_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockRank_SwiftUIApp.swift
+//  StockRank-SwiftUI
+//
+//  Created by JunHyuk Lim on 31/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct StockRank_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
